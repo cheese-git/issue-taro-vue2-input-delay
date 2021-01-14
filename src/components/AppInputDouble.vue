eca5d9c8142852603d6323e3fcd0bd6c3d0349be
cheese <zhenxuan.han@tenoke.com>
2021-01-14 11:34:44 +0800

    add demo

---
+++ src/components/AppInputDouble.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppInput :value="value" @input="$emit('input', $event)" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppInput from './AppInput.vue'
+
+export default Vue.extend({
+  props: {
+    value: String
+  },
+  components: {
+    AppInput
+  }
+})
+</script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,8 @@
 <template>
   <view class="index">
     <view
-      >测试方式：使用安卓手机（如小米10）在输入框中快速输入一串数字，如：123456789</view
+      >测试方式：使用安卓手机（如小米10）在输入框中快速输入一串数字（如：123456789），然后等待
+      1 ~ 2 秒</view
     >
     <van-divider contentPosition="center">会出现卡顿</van-divider>
     <view>封装了 van-field 的自定义组件，通过 v-model 进行双向绑定：</view>
@@ -9,12 +10,17 @@
     <view>封装了 van-field 的自定义组件，通过监听事件进行双向绑定：</view>
     <VanInput :value="value2" @input="value2 = $event" />
     <van-divider contentPosition="center">不会出现卡顿</van-divider>
-    <view>单独的 van-field，通过 v-model 进行双向绑定：</view>
+    <view>直接使用 van-field，通过 v-model 进行双向绑定：</view>
     <van-field v-model="value3" />
     <view>封装了小程序原生输入框的自定义组件，通过 v-model 进行双向绑定：</view>
     <AppInput v-model="value4" />
     <view>小程序原生输入框，通过 v-model 进行双向绑定：</view>
     <input v-model="value5" />
+    <view
+      >对封装了小程序原生输入框的组件再次封装，并通过 v-model
+      进行双向绑定：</view
+    >
+    <AppInputDouble v-model="value6" />
   </view>
 </template>
 
@@ -22,6 +28,7 @@
 import Vue from "vue";
 import './index.css'
 import AppInput from '../../components/AppInput.vue'
+import AppInputDouble from '../../components/AppInputDouble.vue'
 import VanInput from '../../components/VanInput.vue'
 
 export default Vue.extend({
@@ -32,10 +39,11 @@ export default Vue.extend({
       value3: '',
       value4: '',
       value5: '',
+      value6: '',
     }
   },
   components: {
-    AppInput, VanInput
+    AppInput, VanInput, AppInputDouble
   }
 })
 </script>
